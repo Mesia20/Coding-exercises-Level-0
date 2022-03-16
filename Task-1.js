3a376fed4b31e1340906eb21723c47f2e545d003
kwanda nkabinde <kwandankabinde@gmail.com>
2022-03-16 19:42:35 +0200

    renaming variables

--- Task-1.js
+++ Task-1.js
@@ -1,8 +1,11 @@
 let x = 0;
-let y = 1;
-console.log(x);
-console.log(y); 
-let x = x+3;
-let y = y+x;
+let y = 1; 
+
 console.log(x);
 console.log(y);
+
+let a = x + 3;
+let b = y + x;
+
+console.log(a);
+console.log(b);
