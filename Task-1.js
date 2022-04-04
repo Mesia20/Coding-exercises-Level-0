d146ecd392a7faa5a91e79e21b9c644e6c7fc55c
kwanda nkabinde <kwandankabinde@gmail.com>
2022-04-04 10:40:56 +0200

    task 1

--- Task-1.js
+++ Task-1.js
@@ -1,11 +1,11 @@
 let x = 0;
-let y = 1; 
+let y = 1;
 
 console.log(x);
 console.log(y);
 
-let a = x + 3;
-let b = y + x;
+x = x + 3;
+y = y + x;
 
-console.log(a);
-console.log(b);
+console.log(x);
+console.log(y);
