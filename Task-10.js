3a22b0a922f5c480f44f6d793ee390e3e9b9627f
kwanda nkabinde <kwandankabinde@gmail.com>
2022-04-06 09:42:35 +0200

    formatted with prettier

--- Task-10.js
+++ Task-10.js
@@ -1,15 +1,15 @@
-function commonChar(a,b){
-    const matchedChars=[];      //The matched characters will be stored 
-    const lowerCaseA=a.toLowerCase(); //This will make sure all inputted words are lowerCase
-    const lowerCaseB= b.toLowerCase();
-    for(i=0;i<lowerCaseA.length;i++){ 
-        for(j=0;j<lowerCaseB.length;j++){
-            if(lowerCaseA[i]==lowerCaseB[j]){
-                matchedChars.push(lowerCaseA[i])
-            }
-        }
+function commonChar(a, b) {
+  const matchedChars = []; //The matched characters will be stored
+  const lowerCaseA = a.toLowerCase(); //This will make sure all inputted words are lowerCase
+  const lowerCaseB = b.toLowerCase();
+  for (i = 0; i < lowerCaseA.length; i++) {
+    for (j = 0; j < lowerCaseB.length; j++) {
+      if (lowerCaseA[i] == lowerCaseB[j]) {
+        matchedChars.push(lowerCaseA[i]);
+      }
     }
-        console.log("Common letters: "+ matchedChars.join(", "));           
-    }
-    
-commonChar("HOUSE","computers")
+  }
+  console.log("Common letters: " + matchedChars.join(", "));
+}
+
+commonChar("HOUSE", "computers");
